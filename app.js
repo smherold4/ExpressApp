41a733ea2d95451dd9ab3805a0cd4af7d0b3fcff
Michael Herold <mherold@buildzoom.com>
2014-12-29 23:29:05 -0800

    updating package

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
-// var expressLayouts = require('express-ejs');
 var path = require('path');
-// var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -20,10 +18,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout', 'layout')
-// app.use(expressLayouts)
 
-// app.use(favicon());
-app.use(logger('dev'));
+=app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
