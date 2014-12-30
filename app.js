476da1375fc64a14fa90adfa73cdccf2a155220b
Michael Herold <mherold@buildzoom.com>
2014-12-29 23:30:36 -0800

    updating package

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout', 'layout')
 
-=app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
