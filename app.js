b559ff3f48818d91ce4451171bfbec27433cbd23
Michael Herold <mherold@buildzoom.com>
2015-08-29 16:38:47 -0700

    leaner app.js

--- app.js
+++ app.js
@@ -8,18 +8,13 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/cheddar');
 
-var base_routes = require('./routes/index');
-var user_routes = require('./routes/users');
-var item_routes = require('./routes/items');
-var api_routes = require('./routes/api/index');
 var app = express();
 
 // view engine setup
-var expressLayouts = require('express-ejs-layouts');
 app.set('views', path.join(__dirname, 'views')); // console.log(__dirname);
 app.set('view engine', 'ejs');
 app.set('layout', 'layout');
-app.use(expressLayouts);
+app.use(require('express-ejs-layouts'));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -32,10 +27,10 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use('/api', api_routes);
-app.use('/users', user_routes);
-app.use('/items', item_routes);
-app.use('/', base_routes);
+app.use('/api', require('./routes/api/index'));
+app.use('/users', require('./routes/users'));
+app.use('/items', require('./routes/items'));
+app.use('/', require('./routes/index'));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
